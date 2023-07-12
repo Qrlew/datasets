19393b65e26278e996e17fe5da15e34ab0433a99
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-07-13 00:04:51 +0200

    Push data

--- datasets/build.py
+++ datasets/build.py
@@ -1,16 +1,15 @@
-from sqlalchemy import create_engine, MetaData, text
-from sqlalchemy.schema import CreateSchema
+from sqlalchemy import create_engine, text, MetaData, Table, Column
+from sqlalchemy import Integer, String
+from sqlalchemy.schema import CreateTable
 from datasets.sources.financial import Financial
 from datasets.databases.postgresql import PostgreSQL
 
 # Get a source db
 input = Financial()
-for table in input.tables():
-    print(table)
+
+print(input)
 
 # Copy to Postgresql
 output = PostgreSQL(port=5432)
-output.add_schema(input.schema())
-for table in input.tables():
-    print(table)
-    table.create(output)
+output.add(input)
+
--- datasets/database.py
+++ datasets/database.py
@@ -1,7 +1,11 @@
 
 from collections.abc import Collection
-from sqlalchemy import Engine, MetaData, Table, Column
-from sqlalchemy.schema import CreateSchema
+from io import StringIO
+import logging
+from sqlalchemy import Engine, MetaData, Table, Column, create_mock_engine, select, insert
+from sqlalchemy.schema import CreateSchema, CreateTable
+
+logger = logging.getLogger(__name__)
 
 class Database:
     def engine(self) -> Engine:
@@ -22,6 +26,15 @@ class Database:
     def tables(self) -> Collection[Table]:
         """Return all the tables"""
         return self.metadata().sorted_tables
+
+    def dump(self) -> str:
+        """Return the CREATE TABLE statement"""
+        result = StringIO()
+        def dump(sql, *multiparams, **params) -> str:
+            return result.write(str(sql.compile(dialect=self.engine().dialect)))
+        engine = create_mock_engine('postgresql+psycopg://', dump)
+        self.metadata().create_all(engine, checkfirst=True)
+        return result.getvalue()
     
 class MutableDatabase(Database):
     def schema(self) -> str:
@@ -30,11 +43,33 @@ class MutableDatabase(Database):
         return self._schema
 
     def add_schema(self, schema: str):
-        with self.engine().connection() as connection:
-            connection.execute(CreateSchema(schema))
+        logger.info(f'Add schema {schema}')
+        with self.engine().connect() as conn:
+            conn.execute(CreateSchema(schema, if_not_exists=True))
+            conn.commit()
+        self.set_schema(schema)
     
     def set_schema(self, schema: str):
         self._schema = schema
         
+    def add(self, source: Database):
+        logger.info(f'Add {source}')
+        self.add_schema(source.schema())
+        with self.engine().connect() as conn:
+            for table in source.tables():
+                # Remove indexes to avoid name collisions
+                table.indexes.clear()
+                # TODO maybe we can rename indexes
+                table.create(conn, checkfirst=True)
+                conn.commit()
+            # Push data
+            with source.engine().connect() as src_conn:
+                for table in source.tables():
+                    logger.info(f'Loading data from {table}')
+                    rows = src_conn.execute(select(table))
+                    for row in rows:
+                        logger.info(row)
+                        conn.execute(insert(table).values(row))
+                    conn.commit()
 
         
