011e597badf2ccaccd097f9dc0e439f2b35ed487
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-07-13 13:32:46 +0200

    3 sources

--- datasets/build.py
+++ datasets/build.py
@@ -1,26 +1,16 @@
 from sqlalchemy import create_engine, text, MetaData, Table, Column
 from sqlalchemy import Integer, String
 from sqlalchemy.schema import CreateTable
-from datasets.sources.financial import Financial
+from datasets.sources.financial.financial import Financial
 from datasets.sources.loader import Loader
 from datasets.databases.postgresql import PostgreSQL
 from datasets.databases.mariadb import MariaDB
 
-# Get a source db
-input = Financial()
+database = PostgreSQL()
+# loader = Loader(database)
+# loader.load(Financial())
 
-# Copy to Postgresql
-# output = PostgreSQL()
-# output.set_schema('financial')
-# output.add(input)
-# output.dump('/tmp/pg_financial')
+# database.set_schema('financial')
+# database.dump('/tmp/financial.sql')
 
-# db = MariaDB()
-# db._dump('relational.fit.cvut.cz', 'guest', 'relational', 'financial', '/tmp/financial.sql')
-
-output = PostgreSQL()
-# output.set_schema('financial')
-# output.load('/tmp/financial.sql')
-
-loader = Loader(output)
-loader.load(input)
+print(database.declaration())
