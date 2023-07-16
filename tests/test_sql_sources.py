6b36aec712047ab0ff5e5bb7f0727e563fb09c8d
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-07-16 09:40:11 +0200

    fixed path

--- tests/test_sql_sources.py
+++ tests/test_sql_sources.py
@@ -1,5 +1,6 @@
 import os.path
 import importlib.resources as pkg_resources
+from datasets import sources
 
 def test_financial():
     assert os.path.exists('financial', pkg_resources.files(sources) / 'financial' / 'financial.sql')
