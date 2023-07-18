186378618fb0d975e62c7d5b2f91563dcb1f3521
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-07-18 18:33:34 +0200

    Changed network

--- datasets/network.py
+++ datasets/network.py
@@ -1,4 +1,5 @@
 import subprocess
+import logging
 
 NAME: str = 'qrlew-net'
 
@@ -6,4 +7,7 @@ class Network:
     """Create a network"""
     def __init__(self, name=NAME):
         self.name = name
-        subprocess.run(['docker', 'network', 'create', self.name])
+        try:
+            subprocess.run(['docker', 'network', 'create', self.name])
+        except:
+            logging.info("docker not installed")
