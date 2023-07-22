72c308a9c7e43c9a402d08e204ac1c470ad39b60
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-07-22 12:00:47 +0200

    0.3

--- examples/simple.py
+++ examples/simple.py
@@ -17,7 +17,7 @@ name = 'pyqrlew-db'
 #     '--publish', '5432:5432',
 #     'postgres'])
 
-db = PostgreSQL(name=name, user='postgres', password=name, port=5432)
+db = PostgreSQL(name=name, user='postgres', password=name, port=5433)
 db.load('/datasets/datasets/sources/extract/extract.sql')
 db.set_schema('extract')
 db.dump('/tmp/test.dump')
